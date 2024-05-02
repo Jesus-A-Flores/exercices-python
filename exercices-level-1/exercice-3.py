6427b7e24d5fbb00126c030888da28116b963385
Jesus <143859436+Jesus-A-Flores@users.noreply.github.com>
2024-05-02 00:30:11 -0400

    Update exercice-3.py

--- exercices-level-1/exercice-3.py
+++ exercices-level-1/exercice-3.py
@@ -1,3 +1,8 @@
 '''Definir una función que calcule la longitud de una lista o una cadena dada. (Es cierto que python
 tiene la función len() incorporada, pero escribirla por nosotros mismos resulta un muy buen
 ejercicio.'''
+def longitud(a):
+  cont = 0
+  for i in a:
+    cont += 1
+  return cont
