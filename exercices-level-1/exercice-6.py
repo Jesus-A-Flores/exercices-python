8c0515d6ceb1716702d4bdb883576873b49aa213
Jesus <143859436+Jesus-A-Flores@users.noreply.github.com>
2024-05-02 00:43:43 -0400

    Update exercice-6.py

--- exercices-level-1/exercice-6.py
+++ exercices-level-1/exercice-6.py
@@ -1,2 +1,7 @@
 '''Definir una función inversa() que calcule la inversión de una cadena. Por ejemplo la cadena
 "estoy probando" debería devolver la cadena "odnaborp yotse"'''
+def inver(a):
+  inv = ""
+  for i in range(len(a)-1,-1,-1):
+    inv+=a[i]
+  return inv
