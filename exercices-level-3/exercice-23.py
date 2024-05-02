91d28975f2f441618c9c9e41ff2b292bce85204d
Jesus <143859436+Jesus-A-Flores@users.noreply.github.com>
2024-05-02 10:39:29 -0400

    Update exercice-23.py

--- exercices-level-3/exercice-23.py
+++ exercices-level-3/exercice-23.py
@@ -2,3 +2,5 @@
  después de que el usuario lo introduzca muestre por 
  pantalla <NOMBRE> tiene <n> letras, donde <NOMBRE> es el nombre de usuario en mayúsculas y <n> es el número 
  de letras que tienen el nombre.'''
+name = input("Ingrese su nombre: ")
+print(f"{name} tiene {name.len()}")
