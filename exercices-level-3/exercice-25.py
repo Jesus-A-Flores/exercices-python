359739d385f9997017e5d8f0e14bb61fb03b22fa
Jesus <143859436+Jesus-A-Flores@users.noreply.github.com>
2024-05-02 10:50:42 -0400

    Update exercice-25.py

--- exercices-level-3/exercice-25.py
+++ exercices-level-3/exercice-25.py
@@ -1,2 +1,4 @@
 '''Escribir un programa que pida al usuario que introduzca una 
 frase en la consola y muestre por pantalla la frase invertida.'''
+frase = input("Ingrese una frase: ")
+print(f"Frase Invertida: {frase.[::-1]}")
