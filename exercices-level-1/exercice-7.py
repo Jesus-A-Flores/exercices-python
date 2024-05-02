83a55492fae8214969a04c5cc41265e8f874b686
Jesus <143859436+Jesus-A-Flores@users.noreply.github.com>
2024-05-02 00:49:39 -0400

    Update exercice-7.py

--- exercices-level-1/exercice-7.py
+++ exercices-level-1/exercice-7.py
@@ -1,2 +1,8 @@
 '''Definir una función es_palindromo() que reconoce palíndromos (es decir, palabras que tienen el
 mismo aspecto escritas invertidas), ejemplo: es_palindromo ("radar") tendría que devolver True.'''
+def es_palindromo(a):
+  inv = a[::-1]
+  if inv == a:
+    return True
+  else:
+    return False
