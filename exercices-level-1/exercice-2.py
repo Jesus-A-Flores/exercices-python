8dbaa41f2fa01889d7846085d68f1e9ca53fd32d
Jesus <143859436+Jesus-A-Flores@users.noreply.github.com>
2024-05-02 00:22:35 -0400

    Update exercice-2.py

--- exercices-level-1/exercice-2.py
+++ exercices-level-1/exercice-2.py
@@ -1,2 +1,9 @@
 '''Definir una función max_de_tres(), que tome tres números como argumentos y devuelva el
 mayor de ellos. '''
+def max_de_tres(a,b,c):
+  if a > b and a > c:
+    return a
+  elif b > a and b > c:
+    return b
+  else:
+    return c
