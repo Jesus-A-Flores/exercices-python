d83dbf5b0f83f53959a8276a32c2a08e221e0f3c
Jesus <143859436+Jesus-A-Flores@users.noreply.github.com>
2024-05-02 10:56:02 -0400

    Update exercice-26.py

--- exercices-level-3/exercice-26.py
+++ exercices-level-3/exercice-26.py
@@ -1,3 +1,6 @@
 '''Escribir un programa que pida al usuario que introduzca una frase en la consola y una vocal,
  y después muestre por pantalla
  la misma frase pero con la vocal introducida en mayúscula'''
+frase = input("La frase: ")
+voc = input("La vocal: ")
+print(f"{frase+' '+voc.upper()}")
