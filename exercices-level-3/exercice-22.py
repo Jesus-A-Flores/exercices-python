cb5e43097037f22951f672dd7877f35b74951fd7
Jesus <143859436+Jesus-A-Flores@users.noreply.github.com>
2024-05-02 10:36:45 -0400

    Update exercice-22.py

--- exercices-level-3/exercice-22.py
+++ exercices-level-3/exercice-22.py
@@ -2,3 +2,8 @@
 y después muestre por pantalla el nombre completo del usuario tres veces, una con todas las letras minúsculas, 
 otra con todas las letras mayúsculas y otra solo con la primera letra del nombre y de los apellidos en mayúscula.
  El usuario puede introducir su nombre combinando mayúsculas y minúsculas como quiera. '''
+fullname = input("Full Name?: ")
+n1 = fullname.upper()
+n2 = fullname.lower()
+n3 = fullname.title()
+print(f"1. {n1}\n2. {n2}\n3. {n3}")
