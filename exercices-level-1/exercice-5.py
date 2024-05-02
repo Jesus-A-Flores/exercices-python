4de28c488e629e181027d5eda51070a600fe50a9
Jesus <143859436+Jesus-A-Flores@users.noreply.github.com>
2024-05-02 00:37:46 -0400

    Update exercice-5.py

--- exercices-level-1/exercice-5.py
+++ exercices-level-1/exercice-5.py
@@ -1,3 +1,13 @@
 '''Escribir una funcion sum() y una función multip() que sumen y multipliquen respectivamente
 todos los números de una lista. Por ejemplo: sum([1,2,3,4]) debería devolver 10 y multip([1,2,3,4])
 debería devolver 24.'''
+def sum(a):
+  sum = 0
+  for i in a:
+    suma+=i
+  return sum
+def multip(a):
+  mult = 1
+  for i in a:
+    mult*=i
+  return mult
