2214f1d683a3442bba99cc07033f024505a992c3
Jesus <143859436+Jesus-A-Flores@users.noreply.github.com>
2024-05-02 00:33:00 -0400

    Update exercice-4.py

--- exercices-level-1/exercice-4.py
+++ exercices-level-1/exercice-4.py
@@ -1,2 +1,7 @@
 '''Escribir una función que tome un carácter y devuelva True si es una vocal, de lo contrario
 devuelve False.'''
+def isVocal(a):
+  for i in "aeiou":
+    if a == i:
+      return True
+  return False
