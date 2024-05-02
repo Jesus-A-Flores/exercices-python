f8369e25d6dda46cab8596b22adcf87c4fa8c74c
Jesus <143859436+Jesus-A-Flores@users.noreply.github.com>
2024-05-02 10:16:55 -0400

    Update exercice-21.py

--- exercices-level-3/exercice-21.py
+++ exercices-level-3/exercice-21.py
@@ -1,3 +1,7 @@
 '''Escribir un programa que pregunte el nombre del usuario en la consola y un número entero e 
 imprima por pantalla en líneas distintas el nombre del usuario tantas veces como 
 el número introducido.'''
+name = input("Ingrese tun nombre: ")
+n = int(input("Ingrese un numero entero: "))
+for i in range(n):
+  print(name)
